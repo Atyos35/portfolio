074113310225782c4f165fc648ede7a84752b805
Atyos35 <valerian.guemene@gmail.com>
2025-06-05 13:12:12 +0200

    deploy

--- migrations/Version20250603123906.php
+++ migrations/Version20250603123906.php
@@ -18,7 +18,7 @@ final class Version20250602InsertInitialUserData extends AbstractMigration
     {
         $passwordHash = password_hash('password', PASSWORD_BCRYPT);
         $this->addSql("
-            INSERT INTO user (email, roles, password, firstname, lastname, job, linkedin, age, city, phone, is_verified)
+            INSERT INTO user (email, roles, password, firstname, lastname, job, linkedin, age, city, phone, is_verified, profile_picture)
             VALUES (
                 'valerian.guemene@gmail.com',
                 '[\"ROLE_USER\"]',
@@ -30,7 +30,8 @@ final class Version20250602InsertInitialUserData extends AbstractMigration
                 34,
                 'Rennes',
                 '0684445191',
-                1
+                1,
+                ''
             );
         ");
 
